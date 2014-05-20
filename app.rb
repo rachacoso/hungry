6a73e5383488601af2c3acc296b98252362fd5c7
Roberto Achacoso <bertoa@gmail.com>
2014-05-20 15:08:01 -0700

    add menu view

--- app.rb
+++ app.rb
@@ -9,29 +9,32 @@ include Mongo # add Mongo namespace
 # for local access
 mongo = MongoClient.new
 db = mongo['hungry']
-menu = db['menu']
-u_coll = db['users']
-o_coll = db['orders']
+menuitems_c = db['menuitems']
+users_c = db['users']
+orders_c = db['orders']
 
-class Menu
-	attr_accessor :
-end
+# class Menu
+# 	attr_accessor :
+# end
 
-class MenuItem < Menu
+# class MenuItem < Menu
 
-end
+# end
 
-class Order
-	attr_accessor :total
-end
+# class Order
+# 	attr_accessor :total
+# end
 
-class User
-	attr_accessor :first, :last
-end
+# class User
+# 	attr_accessor :first, :last
+# end
 
+# display menu
+get '/menu' do 
 
-get '/add' do 
-
-	coll.insert({name: 'foo'})
-	'done!'
+	# get items by type
+	@appitems = menuitems_c.find({type: "appetizer"})
+	@mainitems = menuitems_c.find({type: "main"})
+	@bevitems = menuitems_c.find({type: "beverage"})
+	erb :menu
 end
