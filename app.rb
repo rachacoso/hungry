fc08a0a45630e922cbd4c872b683e17fb299a39c
Roberto Achacoso <bertoa@gmail.com>
2014-05-30 17:23:08 -0700

    Add remote DB, Add completed orders view, clean up UI

--- app.rb
+++ app.rb
@@ -7,7 +7,24 @@ include Mongo # add Mongo namespace
 enable :sessions
 
 
-#cookies test
+
+
+
+# for MongoHQ access
+# mongodb://<user>:<password>@kahana.mongohq.com:10086/hungry
+mongo = Mongo::MongoClient.from_uri('mongodb://hungry:hungry@kahana.mongohq.com:10086/hungry')
+# mongo = MongoClient.new
+db = mongo['hungry']
+menuitems_c = db['menuitems']
+orders_c = db['orders']
+
+# if accessing an externally hosted DB, do the following (the URI may be different depending on the mongoDB service used)
+# mongo = Mongo::MongoClient.from_uri('mongodb://[username]:[password]@[db.domain.name]:[port]/[dbname]')
+# Here is an example:
+# mongo = Mongo::MongoClient.from_uri('mongodb://berto:xxx@oceanic.mongohq.com:10005/todo')
+
+
+#cookies tests
 get '/foo' do
 	session['m'] = 'Hello World!'
 	session['order'] = 'this is an order'
@@ -38,12 +55,6 @@ end
 
 
 
-# for local access
-mongo = MongoClient.new
-db = mongo['hungry']
-menuitems_c = db['menuitems']
-users_c = db['users']
-orders_c = db['orders']
 
 
 class Hash
@@ -136,11 +147,11 @@ get '/deleteorderitem' do
 
 	if params[:itemid]
 		order_in_progress = orders_c.find("_id" => BSON::ObjectId(session['order'].to_s)).first # get order
-		order_in_progress.except(params[:itemid])
+		order_in_progress.except(params[:itemid]) # remove item from order
 		orders_c.save(order_in_progress) # save order
 
 
-		if order_in_progress.length <= 1 # delete order if no more items in it and reset cookie
+		if order_in_progress.length <= 1 # delete order & reset cookie if no more items in order
 			orders_c.remove("_id" => BSON::ObjectId(session['order'].to_s))
 			session['order'] = nil
 		end
@@ -151,6 +162,81 @@ get '/deleteorderitem' do
 	redirect to('/menu')
 
 end
+
+# DELETE order
+get '/deleteorder' do
+
+	if params[:orderid]
+		orders_c.remove("_id" => BSON::ObjectId(params[:orderid].to_s))
+	end
+	
+	redirect to('/vieworders')
+
+end
+
+# COMPLETE order
+
+post '/completeorder' do
+
+	order_in_progress = orders_c.find("_id" => BSON::ObjectId(session['order'].to_s)).first # get order
+
+	order_in_progress['complete'] = [
+		params[:name],
+		params[:address1],
+		params[:address2],
+		params[:city],
+		params[:state],
+		params[:zip]
+	]
+		
+	orders_c.save(order_in_progress) # save order
+	session['order'] = nil
+
+	@user_complete = order_in_progress['complete']
+
+	erb :thankyou
+
+end
+
+
+
+# VIEW complete orders
+
+get '/vieworders' do
+
+	complete_orders = orders_c.find({ "complete" => { "$exists" => true }}) # find complete orders
+
+	puts complete_orders
+	@complete_orders_list = Array.new # arrray of orders for display
+	
+
+	complete_orders.each do |item|
+
+		this_order = Hash.new
+		this_order['order_total'] = 0
+		this_order['items'] = []
+		
+		item.each do |k,v|
+			if k == '_id' # order id
+				this_order['orderid'] = v
+			elsif k == 'complete' # order delivery info
+				this_order['recipient'] = [ v[0], v[1], v[2], v[3], v[4], v[5] ]
+			else # item info and order total
+				this_item = menuitems_c.find( "_id" => BSON::ObjectId(k) ).first
+				item_price_total = v * this_item['price']
+				this_order['items'] << [ this_item['name'], v.to_s , this_item['price'] ,  item_price_total.to_f ]
+				this_order['order_total'] += item_price_total.to_f
+			end
+		end
+
+		@complete_orders_list << this_order
+
+	end
+
+	erb :vieworders
+
+end
+
 
 # EDIT the menu
 
@@ -210,4 +296,6 @@ get '/deletemenu' do
 		redirect to('/editmenu')
 	end
 end
+
+
 
